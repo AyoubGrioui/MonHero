623b8d396dfaa04ef55c2d6f9d5585a981aa9025
AyoubGrioui <griouiayoub01@gmail.com>
2021-12-21 16:41:55 +0100

    first commit

---
+++ Hero.java
@@ -0,0 +1,12 @@
+
+public Class Hero{
+	
+	
+	public static void main(){
+		String hero  = "BakaChan";
+		System.out.println("I am Your Hero ! " + hero);
+		int power  = 100;
+		System.out.println("My Power is " +power);
+	}
+	
+}
