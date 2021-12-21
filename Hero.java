72f559a9b37384656dafdb86c76514c3e59327ca
AyoubGrioui <griouiayoub01@gmail.com>
2021-12-21 16:46:08 +0100

    second commit

--- Hero.java
+++ Hero.java
@@ -5,7 +5,7 @@ public Class Hero{
 	public static void main(){
 		String hero  = "BakaChan";
 		System.out.println("I am Your Hero ! " + hero);
-		int power  = 100;
+		int power  = 99;
 		System.out.println("My Power is " +power);
 	}
 	
